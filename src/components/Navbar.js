d7cf622a1c72293325b39d8e018976e9e8dbf4de
Brandon <brandonbgrayson@gmail.com>
2021-11-05 20:22:55 -0400

    imported imprint logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {extendTheme} from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 import { Box } from "@chakra-ui/react"
 import "../css/Navbar.css"
-
+import { AspectRatio } from "@chakra-ui/react"
+import { Image } from "@chakra-ui/react"
 
 
 export default function Navbar() {
     return (
-        <Box w="100%" h="100%" bg="#2F303A" className="default-styles">    
-            <h1>This is the navbar</h1>
+        <Box w="100%" h="100%" bg="#2F303A" className="default-styles">
+            <AspectRatio maxW="4em" ratio={4 / 3}>
+                <Image src="/img/imprint-logo.jpeg" alt="naruto" objectFit="cover" />
+            </AspectRatio>
         </Box>
     )
 }
