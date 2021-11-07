0bc8e998698df472b4193553f70c302b853b9ffd
Brandon <brandonbgrayson@gmail.com>
2021-11-07 17:07:51 -0500

    styled Imprint and Disover links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Link } from '@chakra-ui/react';
 import '../css/Navbar.css';
 import { Image } from '@chakra-ui/react';
 import { List, ListItem, ListIcon, OrderedList, UnorderedList } from "@chakra-ui/react"
@@ -9,11 +9,24 @@ export default function Navbar() {
         <header>
             <Box w="100%" h="100%" bg="#2F303A" className="default-styles">
                 <Box display="flex">
-                    <Image src="/img/imprint-logo.jpeg" alt="naruto" objectFit="cover" boxSize="2em" />
-                    <Box>Imprint</Box>
+                    <Image
+                     src="/img/imprint-logo.jpeg" 
+                     alt="naruto" 
+                     objectFit="cover" 
+                     boxSize="2em" 
+                     mx="1rem"
+                     />
+                    <Link
+                     mx=".1rem" 
+                     my=".2rem" 
+                     _hover={{textDecoration: 'none', color: "#ababab"}}
+                     >Imprint
+                     </Link>
                 </Box>
-                <nav>
-
+                <nav className="main-nav">
+                    <UnorderedList display="flex">
+                        <Link _hover={{textDecoration: 'none', color: "#ababab"}} > Discover </Link>
+                    </UnorderedList>
                 </nav>
             </Box>
         </header>
