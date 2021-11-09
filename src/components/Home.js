470f755b5c6c24ab3bb88dc4e966ba3661671803
Brandon <brandonbgrayson@gmail.com>
2021-11-09 15:15:30 -0500

    styled lading page img and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { Box, Image, AspectRatio, Text, Button } from '@chakra-ui/react';
+import { Box, Image, Text, Button } from '@chakra-ui/react';
 
 // flex grow worked in this box: style={{flexGrow: 1, backgroundColor: 'red'}}
 
 export default function Home() {
-	return (
-		<Box>
-			<Box>
-				<AspectRatio maxW="400px" ratio={4 / 6}>
-					<Image src="/img/landing-page-girl.png" alt="girl enjoying music" objectFit="cover" />
-				</AspectRatio>
-			</Box>
-			<Box>
-				<Text fontSize="4xl">Be Inspired</Text>
-				<Text fontSize="md">Dedicated to people who left their mark</Text>
-				<Button size="md" bg="#1762A7" variant="solid">
-					Enter
+    return (
+        <Box
+            d="flex"
+            alignItems="stretch"
+            justifyContent="space-between"
+            flexGrow="1"
+            >
+            <Box
+             d="flex"
+             flexGrow="1"
+             maxH="70vh"
+             alignSelf="end"
+            >
+                <Image
+                 src="/img/landing-page-girl.png" 
+                 alt="girl enjoying music" 
+                 objectFit="contain"
+                 />
+            </Box>
+            <Box
+             d="flex"
+             flexDirection="column"
+             alignItems="flex-start"
+             alignSelf="center"  
+             m="1rem"
+             maxW="100%"
+             flexGrow="2"
+
+             >
+                <Text pb=".5rem" fontWeight="bold" fontSize="4xl">Be Inspired</Text>
+                <Text pb="1rem" fontWeight="normal" fontSize="md">Dedicated to people who left their mark</Text>
+                <Button size="md" bg="#1762A7" variant="solid">
+                    Enter
 				</Button>
-			</Box>
-		</Box>
-	);
+            </Box>
+        </Box>
+    );
 }
