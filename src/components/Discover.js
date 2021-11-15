949f2bdcd44ba1d4e9ffb96dcdfe75f443190441
Brandon <brandonbgrayson@gmail.com>
2021-11-15 00:17:55 -0500

    added Aspect Ratio with iframe for Discover component

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,7 +1,21 @@
 import React from "react"
+import { AspectRatio } from "@chakra-ui/react"
+
+// https://www.youtube.com/embed/g3mVwt0B6G4
 
 export default function Discover() {
     return (
-        <h1>Discover</h1>
+        <div>
+            <AspectRatio 
+            maxW="90vw" 
+            maxH="90vh"
+            ratio={1}>
+                <iframe
+                    title="Video Player"
+                    src="https://www.youtube.com/embed/fb-K-3arN9c"
+                    allowFullScreen
+                />
+            </AspectRatio>
+        </div>
     )
 }
