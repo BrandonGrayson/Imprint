4ca3e95a57328d6b14c0c6e856609a10ce49067d
Brandon <brandonbgrayson@gmail.com>
2021-12-01 10:41:32 -0500

    able to use index variable and template strings to visit blog/:id

--- src/components/ChildPost.js
+++ src/components/ChildPost.js
@@ -1,14 +1,15 @@
 import React from "react"
-import {useParams } from "react-router-dom"
-import {Box, Text} from "@chakra-ui/react"
+import { useParams } from "react-router-dom"
+import { Box, Text } from "@chakra-ui/react"
+
 
 export default function ChildPost() {
 
-    const {id} = useParams()
+    const { id } = useParams()
     return (
         <Box>
             <Text>Child Post Component</Text>
-    <Text>We are at Post Number {id}</Text>
+            <Text>We are at Post Number {id}</Text>
         </Box>
     )
 }
--- src/components/MainPost.js
+++ src/components/MainPost.js
@@ -1,33 +1,37 @@
 import React from 'react'
-import { Box, Text, Button } from "@chakra-ui/react"
+import { Box, Text, Link } from "@chakra-ui/react"
+import { UnorderedList, Button } from "@chakra-ui/react"
 // import fetchPost from '../api/fetchPosts'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { Link as RouteLink} from "react-router-dom"
 
-const postIds = [1,2,3,4,5,6,7,8]
+const postIds = [1, 2, 3, 4, 5, 6, 7, 8]
 
-function fetchPost (id) {
-  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    .then((res) => res.json())
+function fetchPost(id) {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((res) => res.json())
 }
 
 export default function MainPost() {
 
     const [post, setPost] = React.useState([])
+    const [index, setIndex] = React.useState(1)
 
     React.useEffect(() => {
         fetchPost(1)
-        .then((res) => setPost(res))
+            .then((res) => setPost(res))
     }, [])
-    
+
     console.log(post)
+    console.log(`Template Literal`)
     return (
         <Box>
             <Text>This is the Main Blog Component</Text>
-            <Link to='/blog/1'>
+            <Link as={RouteLink} to={`/blog/${index}`}>
                 <Button>View Post</Button>
             </Link>
 
-    <Text>{post.title}</Text>
+            <Text>{post.title}</Text>
 
             <Outlet />
         </Box>
