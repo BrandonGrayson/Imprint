1f1126fec1d7a779565f606c0601a945ef229004
Brandon <brandonbgrayson@gmail.com>
2021-12-01 10:06:46 -0500

    Created Nested Route Tree for Posts and able to manually navigate to a specific post

--- src/components/ChildPost.js
+++ src/components/ChildPost.js
@@ -3,9 +3,12 @@ import {useParams } from "react-router-dom"
 import {Box, Text} from "@chakra-ui/react"
 
 export default function ChildPost() {
+
+    const {id} = useParams()
     return (
         <Box>
             <Text>Child Post Component</Text>
+    <Text>We are at Post Number {id}</Text>
         </Box>
     )
 }
--- src/components/MainPost.js
+++ src/components/MainPost.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { Box, Text, Button } from "@chakra-ui/react"
-import fetchPost from '../api/fetchPosts'
-import { Outlet } from 'react-router-dom'
+// import fetchPost from '../api/fetchPosts'
+import { Link, Outlet } from 'react-router-dom'
+
+const postIds = [1,2,3,4,5,6,7,8]
+
+function fetchPost (id) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((res) => res.json())
+}
 
 export default function MainPost() {
+
+    const [post, setPost] = React.useState([])
+
+    React.useEffect(() => {
+        fetchPost(1)
+        .then((res) => setPost(res))
+    }, [])
     
+    console.log(post)
     return (
         <Box>
             <Text>This is the Main Blog Component</Text>
-            <Button bg="blue.400">Get Post</Button>
+            <Link to='/blog/1'>
+                <Button>View Post</Button>
+            </Link>
+
+    <Text>{post.title}</Text>
 
             <Outlet />
         </Box>
